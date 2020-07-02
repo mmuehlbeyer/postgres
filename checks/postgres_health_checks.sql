92ee157dac93ed67aee735eea7b64bba7114e51b
mmuehlbeyer <m.muehlbeyer@gmx.de>
2020-07-02 08:22:50 +0200

    changed postgres healt check

--- checks/postgres_health_checks.sql
+++ checks/postgres_health_checks.sql
@@ -121,6 +121,22 @@ where not indisunique  and idx_scan < 50   and pg_relation_size(relid) > 5 * 819
 order by pg_relation_size(i.indexrelid) / nullif(idx_scan, 0) desc nulls first,
 pg_relation_size(i.indexrelid) desc;
 
+SELECT s.schemaname,
+       s.relname AS tablename,
+       s.indexrelname AS indexname,
+       pg_relation_size(s.indexrelid) AS index_size,
+       s.idx_scan
+FROM pg_catalog.pg_stat_user_indexes s
+   JOIN pg_catalog.pg_index i ON s.indexrelid = i.indexrelid
+WHERE s.idx_scan < 10      -- has never been scanned/not scanned for more than 10 times
+  AND 0 <>ALL (i.indkey)  -- no index column is an expression
+  AND NOT i.indisunique   -- is not a UNIQUE index
+  AND NOT EXISTS          -- does not enforce a constraint
+         (SELECT 1 FROM pg_catalog.pg_constraint c
+          WHERE c.conindid = s.indexrelid)
+ORDER BY pg_relation_size(s.indexrelid) DESC;
+
+
 
 
 -- last autovacuum run
---
+++ pg_get_ddl.sql
@@ -0,0 +1,12 @@
+--postgresql get ddl collection
+-- get ddl for several objects
+
+
+-- get ddl for function
+select proname
+     , pg_get_functiondef(a.oid)
+  from pg_proc a
+ where a.proname like '%refresh_%';
+
+
+ 
