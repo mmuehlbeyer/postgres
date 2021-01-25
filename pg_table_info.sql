34488a729cb7e3026cb907d8bc394517e995bddf
mmuehlbeyer <m.muehlbeyer@gmx.de>
2021-01-25 21:53:33 +0100

    changed pg_table_info.sql

--- pg_table_info.sql
+++ pg_table_info.sql
@@ -6,7 +6,8 @@
 select  table_schema,table_name,column_name,data_type, character_maximum_length
 from information_schema.columns 
 where
--- table_schema='pe' and 
+-- optionally limit ouput to specific schema
+-- table_schema='myschema' and 
 data_type='character varying'
 and table_schema not in ('information_schema')
 order by table_schema
