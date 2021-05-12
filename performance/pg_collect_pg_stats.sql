6b056e9014bd69705e5d5ba9edf75af18df55f96
mmuehlbeyer <mmuehlbeyer@users.noreply.github.com>
2021-05-12 09:45:46 +0200

    move pg_collect

---
+++ performance/pg_collect_pg_stats.sql
@@ -0,0 +1,15 @@
+/*collect postgres performance data
+from pg_stat_statements or saved pg_stat_statements
+query output with "" for CSV imports to Excel
+all times in seconds
+orderd descending by mean_time 
+*/
+
+--select all queries with INSERT statements
+select queryid,'"'||query||'"',calls,(total_time/1000) as total_time ,(min_time/1000) as min_time,(max_time/1000) as max_time,(mean_time/1000) as mean_time from pg_stat_statements where query like 'INSERT%' order by mean_time desc;
+
+
+--select all queries with SELECT statements
+select queryid,'"'||query||'"',calls,(total_time/1000) as total_time ,(min_time/1000) as min_time,(max_time/1000) as max_time,(mean_time/1000) as mean_time from pg_stat_statements where query like 'SELECT%' order by mean_time desc;
+
+
