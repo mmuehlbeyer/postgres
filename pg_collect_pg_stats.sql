bc01f209b1b9f96d7723151861f69a22f39f27a1
mmuehlbeyer <mmuehlbeyer@users.noreply.github.com>
2021-05-12 09:41:34 +0200

    adapted table name

--- pg_collect_pg_stats.sql
+++ pg_collect_pg_stats.sql
@@ -6,10 +6,10 @@ orderd descending by mean_time
 */
 
 --select all queries with INSERT statements
-select queryid,'"'||query||'"',calls,(total_time/1000) as total_time ,(min_time/1000) as min_time,(max_time/1000) as max_time,(mean_time/1000) as mean_time from pg_stats_mm_230420 where query like 'INSERT%' order by mean_time desc;
+select queryid,'"'||query||'"',calls,(total_time/1000) as total_time ,(min_time/1000) as min_time,(max_time/1000) as max_time,(mean_time/1000) as mean_time from pg_stat_statements where query like 'INSERT%' order by mean_time desc;
 
 
 --select all queries with SELECT statements
-select queryid,'"'||query||'"',calls,(total_time/1000) as total_time ,(min_time/1000) as min_time,(max_time/1000) as max_time,(mean_time/1000) as mean_time from pg_stats_mm_230420 where query like 'SELECT%' order by mean_time desc;
+select queryid,'"'||query||'"',calls,(total_time/1000) as total_time ,(min_time/1000) as min_time,(max_time/1000) as max_time,(mean_time/1000) as mean_time from pg_stat_statements where query like 'SELECT%' order by mean_time desc;
 
 
