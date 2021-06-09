e14c74998c57a7b48a760df14aaef06a7c28edf0
mmuehlbeyer <mmuehlbeyer@users.noreply.github.com>
2021-06-09 11:50:47 +0200

    changed interval to days

--- pg_stats_snapshots/pg_stat_clean.sh
+++ pg_stats_snapshots/pg_stat_clean.sh
@@ -5,4 +5,4 @@ export PGDATABASE=db01
 export PGPORT=20001
 
 # cleanup older than 2 month
-psql -c "delete from stat_statements_snapshots sss where sss.created <= (date_trunc('MONTH',current_date) - interval '2 MONTH')::date" >>/tmp/pg_stat_cleanup.log
+psql -c "delete from stat_statements_snapshots sss where sss.created <= (date_trunc('DAYS',current_date) - interval '30 DAYS')::date" >>/tmp/pg_stat_cleanup.log
